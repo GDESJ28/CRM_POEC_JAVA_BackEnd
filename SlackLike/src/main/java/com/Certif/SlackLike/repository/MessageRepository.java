9094025e7354e9037349b6ddd5cfacb03763f3a1
Gilles Desjardins <gilles.desj@gmail.com>
2023-12-01 16:47:03 +0100

    creation des interfaces Repository

---
+++ SlackLike/src/main/java/com/Certif/SlackLike/repository/MessageRepository.java
@@ -0,0 +1,8 @@
+package com.Certif.SlackLike.repository;
+
+import com.Certif.SlackLike.model.Message;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+}
